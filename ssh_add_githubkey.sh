300cda2ad8913f2b16464cb6e1e1a26e73cd9bbd
ian-revolt <69251514+ian-revolt@users.noreply.github.com>
2020-08-05 12:13:40 -0400

    Update ssh_add_githubkey.sh

--- ssh_add_githubkey.sh
+++ ssh_add_githubkey.sh
@@ -13,10 +13,10 @@ echo Hello, what is the GitHub user name?
 read vargit
 
 # check if user home dir exists, create if it doesn'
-DIR="/home/$varname"
+DIR="/home/"$varname""
 if [! -d "$DIR" ]; then
     echo "Home directory doesn't exist"
 else 
   # add key to authorized_keys from GitHub account
-  wget -O https://github.com/$vargit.keys | sudo tee -a /home/$varname/.ssh/authorized_keys
+  wget -O https://github.com/"$vargit".keys | sudo tee -a /home/"$varname"/.ssh/authorized_keys
 fi 
